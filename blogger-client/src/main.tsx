bd443ee8cc76af53a83fc42d365e2436886b8a5e
Nico Rithner <nicorithner@gmail.com>
2024-06-30 15:27:08 -0600

    Add axios default base url

--- blogger-client/src/main.tsx
+++ blogger-client/src/main.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import axios from "axios";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+axios.defaults.baseURL =
+  import.meta.env.VITE_APP_API_URL || "http://127.0.0.1:3000";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-)
+);
