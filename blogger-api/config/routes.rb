39fb9e1a1bf422bf8f9f0e48a86b564d3a2a076c
Nico Rithner <nicorithner@gmail.com>
2024-06-30 15:56:46 -0600

    Update routes

--- blogger-api/config/routes.rb
+++ blogger-api/config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  if ENV['RAILS_SERVE_STATIC_FILES'].present?
+    get '*path', to: static('index.html')
+    post '*path', to: static('index.html')
+  end
+
   resources :posts
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
